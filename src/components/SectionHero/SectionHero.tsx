48cc54655380ec195d0baad6ed799cc97cb5988f
Rashid Shaikh <contactrashid8@gmail.com>
2022-04-04 15:15:28 +0530

    updated

--- src/components/SectionHero/SectionHero.tsx
+++ src/components/SectionHero/SectionHero.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import ButtonPrimary from "shared/Button/ButtonPrimary";
-import imagePng from "images/hero-right.png";
+import imagePng from "images/cargoShips.jpg";
 import HeroSearchForm from "components/HeroSearchForm/HeroSearchForm";
 
 export interface SectionHeroProps {
@@ -16,13 +16,9 @@ const SectionHero: FC<SectionHeroProps> = ({ className = "" }) => {
       <div className="flex flex-col lg:flex-row lg:items-center">
         <div className="flex-shrink-0 lg:w-1/2 flex flex-col items-start space-y-8 sm:space-y-10 pb-14 lg:pb-64 xl:pr-14 lg:mr-10 xl:mr-0">
           <h2 className="font-medium text-4xl md:text-5xl xl:text-7xl !leading-[114%] ">
-            Hotel, car & experiences
+            One Globe, One Click
           </h2>
-          <span className="text-base md:text-lg text-neutral-500 dark:text-neutral-400">
-            Accompanying us, you have a trip full of experiences. With Chisfis,
-            booking accommodation, resort villas, hotels
-          </span>
-          <ButtonPrimary>Start your search</ButtonPrimary>
+          {/* <ButtonPrimary>Start your search</ButtonPrimary> */}
         </div>
         <div className="flex-grow">
           <img className="w-full" src={imagePng} alt="hero" />
